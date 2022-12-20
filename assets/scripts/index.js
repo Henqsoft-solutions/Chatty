bdaf0709dd7a47ebd7bda62db6c178ef9cd4ec21
Kachi luiz <samcerick5@gmail.com>
2022-12-20 21:06:07 +0100

    kachi is working on another branch

---
+++ assets/scripts/index.js
@@ -0,0 +1,12 @@
+// SCRIPT FOR LOADER
+
+    var myVar;
+    
+    function myFunction() {
+    myVar = setTimeout(showPage, 2000);
+    }
+
+     function showPage() {
+     document.getElementById("loader").style.display = "none";
+     document.getElementById("myDiv").style.display = "block";
+      }
