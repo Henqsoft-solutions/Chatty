ed8a73914c55e61d36f2d3dfd72ce29f6faa5719
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2022-12-21 19:29:26 +0100

    #Adedoyin-Emmanuel updated the folder structure
    I renamed the include -> includes and edited the loaderscripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,11 @@
 // SCRIPT FOR LOADER
+var myVar;
 
-    var myVar;
-    
-    function myFunction() {
+function myFunction() {
     myVar = setTimeout(showPage, 2000);
-    }
+}
 
-     function showPage() {
-     document.getElementById("loader").style.display = "none";
-     document.getElementById("myDiv").style.display = "block";
-      }
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
